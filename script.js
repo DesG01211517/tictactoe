84f6a846ab9c41125c967bf00094a4d82bcca57b
DesG01211517 <desmongarrison2@gmail.com>
2024-03-03 15:12:53 -0500

    complete

--- script.js
+++ script.js
@@ -120,52 +120,52 @@ R1.onclick = function() {
   checkWinner.onclick = function() {
   
     if (b[0] === "X" && b[1] === "X" && b[2] === "X"){
-      alert("you win");
+      alert("X wins");
     } 
     if (b[0] === "O" && b[1] === "O" && b[2] === "O"){
-      alert("you win");
+      alert("O Wins!");
     }
     if (b[3] === "X" && b[4] === "X" && b[5] === "X"){
-      alert("you win");
+      alert("X wins!");
     }
     if (b[3] === "O" && b[4] === "O" && b[5] === "O"){
-      alert("you win");
+      alert("O Wins!");
     }
     if (b[6] === "X" && b[7] === "X" && b[8] === "X"){
-      alert("you win");
+      alert("X wins!");
     }
     if (b[6] === "O" && b[7] === "O" && b[8] === "O"){
-      alert("you win");
+      alert("O Wins!");
     }
     if (b[0] === "X" && b[4] === "X" && b[8] === "X"){
-      alert("you win");
+      alert("X wins!");
     }
     if (b[0] === "O" && b[4] === "O" && b[8] === "O"){
-      alert("you win");
+      alert("O Wins!");
     }
     if (b[2] === "X" && b[4] === "X" && b[6] === "X"){
-      alert("you win");
+      alert("X wins!");
     }
     if (b[2] === "O" && b[4] === "O" && b[6] === "O"){
-      alert("you win");
+      alert("O Wins!");
     }
     if (b[0] === "X" && b[3] === "X" && b[6] === "X"){
-      alert("you win");
+      alert("X wins!");
     }
     if (b[0] === "O" && b[3] === "O" && b[6] === "O"){
-      alert("you win");
+      alert("O Wins!");
     }
     if (b[1] === "X" && b[4] === "X" && b[7] === "X"){
-      alert("you win");
+      alert("X wins!");
     }
     if (b[1] === "O" && b[4] === "O" && b[7] === "O"){
-      alert("you win");
+      alert("O Wins!");
     }
     if (b[2] === "X" && b[4] === "X" && b[8] === "X"){
-      alert("you win");
+      alert("X wins!");
     }
     if (b[2] === "O" && b[4] === "O" && b[8] === "O"){
-      alert("you win");
+      alert("O Wins!");
     }
   
   }
